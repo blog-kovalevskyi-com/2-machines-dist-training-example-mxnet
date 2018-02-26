cefb694fd6a12147e7d35827455a50835235ba38
Ubuntu <ubuntu@ip-172-31-89-150.ec2.internal>
2018-02-26 01:50:26 +0000

    2w2s async folder added

---
+++ 2_w_2_s_dist_async/server.py
@@ -0,0 +1,11 @@
+import os
+os.environ.update({
+  "DMLC_ROLE": "server",
+  "DMLC_PS_ROOT_URI": "172.31.89.150",
+  "DMLC_PS_ROOT_PORT": "9000",
+  "DMLC_NUM_SERVER": "2",
+  "DMLC_NUM_WORKER": "2",
+  "PS_VERBOSE": "0"
+})
+import mxnet
+kv_store = mxnet.kv.create('dist_async')
---
+++ 2_w_2_s_dist_async/start.sh
@@ -0,0 +1,14 @@
+source activate mxnet_p36
+python scheduler.py &
+python server.py &
+python image-classification/train_imagenet.py \
+--gpu 0,1,2,3,4,5,6,7 --batch-size 960 --num-epochs 10 \
+--data-nthreads 40 --disp-batches 20 \
+--network resnet-v1 --num-layers 50 \
+--max-random-shear-ratio 0 \
+--min-random-scale 0.533 \
+--max-random-rotate-angle 0 \
+--max-random-h 0 --max-random-l 0 \
+--max-random-s 0 --max-random-aspect-ratio 0 \
+--kv-store dyst_async \
+--benchmark 1 &
